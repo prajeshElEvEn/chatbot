30739efab49f602b7a3b1152428a586015440748
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-24 17:05:29 +0530

    fixed response error

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,17 @@
+from tensorflow.keras.models import load_model
+from nltk.stem import WordNetLemmatizer
 import random
 import json
 import pickle
 import numpy as np
 
 import nltk
-from nltk.stem import WordNetLemmatizer
-
-from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
 
+intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-
 model = load_model('chatbot_model.h5')
 
 
