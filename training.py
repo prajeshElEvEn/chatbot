f1aa8ab1ba14aa1dc06dca8209ffb5cc26ea534a
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-24 15:51:34 +0530

    lammetized words

--- training.py
+++ training.py
@@ -9,9 +9,10 @@ import numpy as np
 
 import nltk
 nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
-
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 
@@ -23,8 +24,13 @@ ignore_letters = ['?', '!', '.', ',']
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
-        words.append(word_list)
+        words.extend(word_list)
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-print(documents)
+# print(documents)
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+print(words)
